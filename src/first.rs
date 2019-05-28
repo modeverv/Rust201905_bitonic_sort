3e1c04c34aab68c6df88010a254171b97f472196
modeverv <modeverv@gmail.com>
2019-05-28 21:31:30 +0900

    2019-05-28 21:31:30

--- src/first.rs
+++ src/first.rs
@@ -16,7 +16,7 @@ pub fn sort(x: &mut [u32], up: bool) {
 fn sub_sort(x: &mut [u32], up: bool) {
     if x.len() > 1 {
         compare_and_swap(x, up);
-        let mid_point = x.len() /2;
+        let mid_point = x.len() / 2;
         sub_sort(&mut x[..mid_point], up);
         sub_sort(&mut x[mid_point..], up);
     }
@@ -26,7 +26,7 @@ fn compare_and_swap(x: &mut [u32], up: bool) {
     let mid_point = x.len() / 2;
     for i in 0..mid_point {
         if (x[i] > x[mid_point + i]) == up {
-            x.swap(i,mid_point+i);
+            x.swap(i, mid_point + i);
         }
     }
 }
@@ -37,6 +37,14 @@ mod tests {
 
     #[test]
     fn sort_u32_ascending() {
-        
+        let mut x = vec![10, 30, 11, 20, 4, 330, 21, 110];
+        sort(&mut x, true);
+        assert_eq!(x, vec![4, 10, 11, 20, 21, 30, 110, 330]);
+    }
+    #[test]
+    fn sort_u32_descending() {
+        let mut x = vec![10, 30, 11, 20, 4, 330, 21, 110];
+        sort(&mut x, false);
+        assert_eq!(x, vec![330, 110, 30, 21, 20, 11, 10, 4]);
     }
 }
